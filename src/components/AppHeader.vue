8b6a69cccd4af1b0707446ee5ffc744ac2d1f2b5
andreaa21 <andrea.zollini@proton.me>
2022-11-15 15:53:35 +0100

    add style to AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,12 +6,74 @@ export default {
 
 <template>
   <header>
-    <div class="container">H</div>
+    <div class="container">
+        <div class="logo">
+            <img src="/dc-logo.png" alt="dc-logo">
+        </div>
+
+        <nav>
+            <ul>
+                <li><a href="#">Characters</a></li>
+                <li><a class="active" href="#">Comics</a></li>
+                <li><a href="#">movies</a></li>
+                <li><a href="#">tv</a></li>
+                <li><a href="#">games</a></li>
+                <li><a href="#">collectibles</a></li>
+                <li><a href="#">videos</a></li>
+                <li><a href="#">fans</a></li>
+                <li><a href="#">news</a></li>
+                <li><a href="#">shop</a></li>
+            </ul>
+        </nav>
+
+    </div>
   </header>
 </template>
 
 
 
-<style>
+<style lang="scss" scoped>
 
+@use '../styles/partials/variables' as *;
+@use '../styles/partials/mixin' as *;
+
+
+
+    header{
+        height: 120px;
+        .container{
+            height: 100%;
+            @include centerFlex('vertical');
+            justify-content: space-between;
+
+            .logo{
+                padding: 1rem 0;
+            }
+            nav{
+                height: 100%;
+                
+            }
+            ul{
+                list-style: none;
+                display: flex;
+                height: 100%;
+                li{
+                    margin: 0 .5rem;
+                }
+                a{
+                    color: $header-link-color;
+                    height: 100%;
+                    @include centerFlex('vertical');
+                    text-decoration: none;
+                    text-transform: uppercase;
+                    font-size: .8rem;
+                    &:hover,
+                    &.active{
+                        color: $main-color;
+                        border-bottom: 4px solid $main-color;
+                    }
+                }
+            }
+        }
+    }
 </style>
