157bf698184a53244b4fc30a2225ae76a720a8d6
andreaa21 <andrea.zollini@proton.me>
2022-11-16 16:54:31 +0100

    add button, card to main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,13 +1,22 @@
 <script>
+
+import ComicCard from './ComicCard.vue';
+
 export default {
-    name: 'AppMain'
+    name: 'AppMain',
+    components:{
+      ComicCard
+    }
 }
 </script>
 
 <template>
 <main>
-  <div class="container">
-    content
+  <div class="container debug cards">
+    <ComicCard />
+  </div>
+  <div class="btn-container debug">
+    <button>load more</button>
   </div>
 </main>
 </template>
@@ -24,5 +33,26 @@ export default {
         color: white;
         min-height: 130px;
         @include centerFlex('vertical');
+        flex-direction: column;
+        .cards{
+          display: flex;
+          flex-wrap: wrap;
+          padding: 2rem;
+          margin-top: 100px;
+        }
+
+        .btn-container{
+          margin: 2rem 0;
+          button{
+            background-color: $main-color;
+            border: none;
+            padding: .5rem 2rem;
+            color: white;
+            text-transform: uppercase;
+
+          }
+        }
+
+
     }
 </style>
---
+++ src/components/ComicCard.vue
@@ -0,0 +1,30 @@
+
+<script>
+export default {
+    name: 'ComicCard'
+
+}
+</script>
+
+<template>
+    <div class="card debug">
+        <div class="card-img">
+
+        </div>
+        <div class="card-text">card</div>
+    </div>
+  
+</template>
+
+
+
+<style lang="scss" scoped>
+    .card{
+        width: calc(100% / 6);
+        min-height: 300px;
+        padding: 1rem;
+        .card-text{
+            text-align: center;
+        }
+    }
+</style>
